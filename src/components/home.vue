7954b4699273ed88c095129382f8185354169098
zhongqi1112 <zhongqi1112@gmail.com>
2020-04-17 23:21:21 -0700

    Update page

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,23 +3,19 @@
     <v-row class="text-center">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          COVID-19 US TODAY
+          COVID-19 U.S. TODAY
         </h1>
-
-        <p class="subheading font-weight-regular">
-          Real-time data of COVID-19
-        </p>
       </v-col>
 
       <v-col
         class="mb-5 red--text"
         cols="12"
       >
-        <p class="subheading font-weight-regular">
+        <p class="font-weight-regular">
           New
         </p>
-        <h2 class="headline font-weight-bold mb-3">
-          {{info.NewConfirmed}}
+        <h2 class="display-3 font-weight-bold mb-3">
+          {{ newConfirmed }}
         </h2>
       </v-col>
 
@@ -30,9 +26,9 @@
         <p class="subheading font-weight-regular">
           Infected
         </p>
-        <h1 class="headline font-weight-bold mb-3">
-          {{ info.TotalConfirmed}}
-        </h1>
+        <h2 class="display-3 font-weight-bold mb-3">
+          {{ totalConfirmed }}
+        </h2>
       </v-col>
 
       <v-col
@@ -42,20 +38,20 @@
         <p class="subheading font-weight-regular">
           Recoverd
         </p>
-        <h2 class="headline font-weight-bold mb-3">
-          {{ info.TotalRecovered}}
+        <h2 class="display-3 font-weight-bold mb-3">
+          {{ totalRecovered }}
         </h2>
       </v-col>
 
       <v-col
-        class="mb-5 blue-grey--text text--darken-3"
+        class="mb-5 blue-grey--text text--darken-4"
         cols="12"
       >
         <p class="subheading font-weight-regular">
           Deceased
         </p>
-        <h2 class="headline font-weight-bold mb-3">
-          {{info.TotalDeaths}}
+        <h2 class="display-3 font-weight-bold mb-3">
+          {{ totalDeaths }}
         </h2>
       </v-col>
 
@@ -63,9 +59,6 @@
         class="mb-5"
         cols="12"
       >
-        <p class="mb-3">
-          Updated: {{info. Date}}
-        </p>
       </v-col>
     </v-row>
   </v-container>
@@ -75,16 +68,24 @@
   export default {
     name: 'Home',
     data: () => ({
-      info: null
+      newConfirmed: 0,
+      totalConfirmed: 0,
+      newRecovered: 0,
+      totalRecovered: 0,
+      newDeaths: 0,
+      totalDeaths: 0
     }),
     beforeCreate () {
-      fetch('https://api.covid19api.com/summary')
+      fetch('https://coronavirus-19-api.herokuapp.com/countries/USA')
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          this.info = data.Countries.find(country => country.Slug == 'united-states')
-        });
+          this.newConfirmed = data.todayCases
+          this.totalConfirmed = data.cases
+          this.totalRecovered = data.recovered
+          this.totalDeaths = data.deaths
+        })
     }
   }
 </script>
