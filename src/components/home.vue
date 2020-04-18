70089808bdd7389eca08ee91a8e16fc3eb531f0b
zhongqi1112 <zhongqi1112@gmail.com>
2020-04-18 14:17:09 -0700

    Deploy

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,7 +3,7 @@
     <v-row class="text-center">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          COVID-19 U.S. TODAY
+          COVID-19 U.S.
         </h1>
       </v-col>
 
