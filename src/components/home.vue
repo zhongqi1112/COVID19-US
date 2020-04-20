578b09d676b55a162a40a81526197db31a5b759a
zhongqi1112 <zhongqi1112@gmail.com>
2020-04-19 19:30:28 -0700

    add states list

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container>
     <v-row class="text-center justify-center">
-      <v-col class="mb-5" cols="12">
+      <v-col class="xs-12" cols="12">
         <p class="display-2 font-weight-bold mb-3">
           COVID-19 U.S.
         </p>
       </v-col>
 
-      <v-col class="mb-5 red--text" cols="12">
+      <v-col class="xs-12 red--text" cols="12">
         <p class="font-weight-regular">
           New
         </p>
@@ -16,7 +16,7 @@
         </h2>
       </v-col>
 
-      <v-col class="mb-5 red--text text--darken-4" cols="12">
+      <v-col class="xs-12 red--text text--darken-4" cols="12">
         <p class="subheading font-weight-regular">
           Confirmed
         </p>
@@ -25,7 +25,7 @@
         </h2>
       </v-col>
 
-      <v-col class="mb-5 green--text" cols="12">
+      <v-col class="xs-12 green--text" cols="12">
         <p class="subheading font-weight-regular">
           Recovered
         </p>
@@ -34,7 +34,7 @@
         </h2>
       </v-col>
 
-      <v-col class="mb-5 blue-grey--text text--darken-3" cols="12">
+      <v-col class="xs-12 blue-grey--text text--darken-3" cols="12">
         <p class="subheading font-weight-regular">
           Deceased
         </p>
@@ -43,7 +43,19 @@
         </h2>
       </v-col>
 
-      <v-col class="mb-5" cols="12">
+      <v-col class="xs-12" cols="12">
+        <v-data-table
+          :mobile-breakpoint="0"
+          :headers="headers"
+          :items="states"
+          :items-per-page="10"
+          :sort-by="['cases']"
+          :sort-desc="[true]"
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+
+      <v-col class="xs-12" cols="12">
         <div>Updated: {{ updatedTime }}</div>
       </v-col>
     </v-row>
@@ -58,12 +70,10 @@
       hoursPerDay: 24,
       yesterdayConfirmed: 0,
       updated: 0,
-      newConfirmed: 0,
-      totalConfirmed: 0,
-      newRecovered: 0,
-      totalRecovered: 0,
-      newDeaths: 0,
-      totalDeaths: 0,
+      newConfirmed: 'Loading',
+      totalConfirmed: 'Loading',
+      totalRecovered: 'Loading',
+      totalDeaths: 'Loading',
       headers: [
         {
           text: 'States',
@@ -72,90 +82,24 @@
           value: 'state',
         },
         { text: 'Confirmed', value: 'cases' },
+        { text: 'Active', value: 'active' },
         { text: 'Test', value: 'tests' },
-        { text: 'Deceased', value: 'deaths' }
+        { text: 'Tests / Million', value: 'testsPerOneMillion' }
       ],
-      states: [
-        {
-          "state": "New York",
-          "cases": 247215,
-          "todayCases": 6174,
-          "deaths": 18298,
-          "todayDeaths": 627,
-          "active": 205030,
-          "tests": 617555,
-          "testsPerOneMillion": 31478
-        },
-        {
-          "state": "New Jersey",
-          "cases": 85301,
-          "todayCases": 3881,
-          "deaths": 4202,
-          "todayDeaths": 132,
-          "active": 79828,
-          "tests": 172543,
-          "testsPerOneMillion": 19426
-        },
-        {
-          "state": "Massachusetts",
-          "cases": 38077,
-          "todayCases": 1705,
-          "deaths": 1706,
-          "todayDeaths": 146,
-          "active": 28253,
-          "tests": 162241,
-          "testsPerOneMillion": 23754
-        },
-        {
-          "state": "Pennsylvania",
-          "cases": 32734,
-          "todayCases": 1003,
-          "deaths": 1237,
-          "todayDeaths": 135,
-          "active": 30847,
-          "tests": 158854,
-          "testsPerOneMillion": 12419
-        },
-        {
-          "state": "California",
-          "cases": 31430,
-          "todayCases": 712,
-          "deaths": 1175,
-          "todayDeaths": 28,
-          "active": 28905,
-          "tests": 246400,
-          "testsPerOneMillion": 6294
-        },
-        {
-          "state": "Michigan",
-          "cases": 31424,
-          "todayCases": 633,
-          "deaths": 2391,
-          "todayDeaths": 83,
-          "active": 25796,
-          "tests": 113800,
-          "testsPerOneMillion": 11429
-        },
-        {
-          "state": "Illinois",
-          "cases": 30357,
-          "todayCases": 1197,
-          "deaths": 1290,
-          "todayDeaths": 31,
-          "active": 28461,
-          "tests": 143318,
-          "testsPerOneMillion": 11178
-        }
-      ]
+      statesList: []
     }),
     computed: {
       updatedTime: function () {
         var time = new Date(this.updated)
         var updatedDate = time.toLocaleDateString("en-US") + ' ' + time.toLocaleTimeString("en-US") + ' PDT'
         return updatedDate
+      },
+      states () {
+        return this.statesList
       }
     },
     created () {
+      this.fetchStatesData()
       this.fetchData()
     },
     methods: {
@@ -183,9 +127,31 @@
           console.error(e)
         }
       },
+      /**
+       * @description Get US State Totals
+       */
+      async fetchStatesData () {
+        try {
+          var statesData = await fetch('https://corona.lmao.ninja/v2/states?sort=cases')
+          var states = await statesData.json()
+          this.statesList = states
+        } catch(e) {
+          console.error(e)
+        }
+      },
       numberWithCommas(n) {
         return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
       }
     }
   }
 </script>
+
+<style>
+.v-application p {
+  margin-bottom: 0px !important;
+}
+
+.v-application .xs-12 {
+  margin-bottom: 0px !important;
+}
+</style>
