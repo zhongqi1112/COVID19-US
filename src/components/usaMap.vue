64114513f4ffc67711bc96b4f037af5e6cfd024c
zhongqi1112 <zhongqi1112@gmail.com>
2020-05-04 13:34:13 -0700

    finish d3 map

--- src/components/usaMap.vue
+++ src/components/usaMap.vue
@@ -1,7 +1,5 @@
 <template>
-<div>
-  <div class="d3-geomap" id="map"></div>
-</div>
+  <div class="d3-geomap" id="mapUs"></div>
 </template>
 
 <script>
@@ -11,134 +9,146 @@ import * as topojson from "topojson";
 export default {
   name: "UsaMap",
   data: () => ({
-
+    //
   }),
-  mounted() {
-    this.createMap()
+  created() {
+    this.createMapUs()
   },
   methods: {
-    async createMap() {
+    async createMapUs() {
       // assign map width and height
+      const ratioWidthHeight = 0.585
       const width = window.innerWidth // Use the window's width
-      const height = Math.ceil(width * 0.585)
+      const height = Math.ceil(width * ratioWidthHeight)
       // assign map color
-      const COLOR_MIN = "Cornsilk";
-      const COLOR_MAX = "Maroon";
+      const colorMin = "Cornsilk"
+      const colorMax = "Maroon"
+      const legendLevel = 10
+      // the size of each indicator
+      const legendBase = width
+      const legendSize = 0.02
+      const legendSpace = 0.002
+      const legendX = 0.83
+      const legendY = 0.7
+      const legendFontSize = Math.ceil(legendBase * 0.02)
+      const legendMax = 60000
+      const legendDomain = [1, 5000, 10000, 30000, legendMax]
       // load map data
-      var earthQuake = []
-      var data = await Promise.all([
+      var loadData = await Promise.all([
         d3.json("/d3-geomap/states-10m.json"),
-        d3.dsv(",", "state-earthquakes.csv", function(d) {
-          earthQuake.push({
-            state: d.States,
-            region: d.Region,
-            total: parseInt(d["Total Earthquakes"])
-          })
+        fetch('https://corona.lmao.ninja/v2/states?sort=cases')
+      ]);
+      var statesJson = await loadData[1].json();
+      // assign map data
+      var mapData = loadData[0];
+      var statesMap = topojson.feature(mapData, mapData.objects.states).features;
+      // assign states data
+      var statesData = [];
+      statesJson.forEach(state => {
+        statesData.push({
+          state: state.state,
+          cases: state.cases
         })
-      ])
-      var us = data[0]
-      console.log(us)
+      });
+      // configuration of color
+      // var casesMax = d3.max(statesData.map(function(d) { return parseInt(d.cases)}));
+      var casesScale = d3.scaleLinear()
+        .domain([1, legendMax])
+        .range([1, legendLevel]); // output
+      var colorScale = d3.scaleLinear()
+        .domain([1, legendLevel])
+        .range([colorMin, colorMax]);
+      colorScale.clamp(true)
+      // add the SVG to the page
+      var svg = d3.select("#mapUs").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+      svg.append("g")
+        .attr("transform", "translate(0, 0)");
+      // configuration of tooltip
+      var tooltip = d3.select("#mapUs").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
       // D3 Projection
       var projection = d3.geoAlbersUsa()
         .translate([width / 2, height / 2]) // translate to center of screen
         .scale(width); // scale things down so see entire US
-
       // Define path generator
       var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
         .projection(projection);  // tell path generator to use albersUsa projection
-
-        // load states data
-        var states = topojson.feature(us, us.objects.states).features;
-
-        var totalMax = d3.max(earthQuake.map(function(d) { return parseInt(d.total)}))
-
-        var totalScale = d3.scaleLog()
-          .domain([1,  totalMax])
-          .range([1, 9]); // output
-
-        var colorScale = d3.scaleLinear()
-          .domain([1, 9])
-          .range([COLOR_MIN, COLOR_MAX]);
-
-        //var color = d3.scaleQuantize([1, 7], d3.schemeBlues[6])
-
-        // add the line chart SVG to the page
-        var svg = d3.select("#map").append("svg")
-          .attr("width", width)
-          .attr("height", height)
-        .append("g")
-          .attr("transform", "translate(0, 0)");
-
-        var tooltip = d3.select("#map").append("div")
-          .attr("class", "tooltip")
-          .style("opacity", 0);
-
-
-        // add states map
-        svg.selectAll(".state")
-          .data(states)
-        .enter().append("path")
-          .attr("class", "state")
-          .attr("fill", function(d) {
-            var current = earthQuake.find(function(element) { return element.state === d.properties.name})
-            if (typeof current !== "undefined") {
-              if (current.total !== 0) {
-                return colorScale(totalScale(current.total))
-              } else {
-                return colorScale(totalScale(1))
-              }
+      // add states map
+      svg.selectAll(".state")
+        .data(statesMap)
+      .enter().append("path")
+        .attr("class", "state")
+        .attr("fill", function(d) {
+          var current = statesData.find(function(element) { return element.state === d.properties.name})
+          if (typeof current !== "undefined") {
+            if (current.cases !== 0) {
+              return colorScale(casesScale(current.cases))
+            } else {
+              return colorScale(casesScale(1))
             }
-          })
-          .attr("d", path)
-          .on("mouseover", function(d) {
-            console.log(d)
-            tooltip.transition()
-            .duration(200)
-            .style("opacity", .9);
-            tooltip.html(d.properties.name)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-          })
-          .on("mouseout", function() {
-            tooltip.transition()
-            .duration(500)
-            .style("opacity", 0);
-          });
-
-          svg.append("path")
-                .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
-                .attr("fill", "none")
-                .attr("stroke", "white")
-                .attr("stroke-linejoin", "round")
-                .attr("d", path);
-          var margin = {top: 50, right: 50, bottom: 50, left: 50}
-          const LINE_HEIGHT = 25;
-          const INDICATOR_SIZE = 20;
-          var totalDomain = [1, 5, 50, 200, 500, 1000, 3000, 6000, 10000]
-          // add legend
-          // add legend title
-          svg.append("text")
-            .attr("y", 8 * margin.top)
-            .attr("x", width - margin.top * 2)
-            .attr("text-anchor", "middle")
-            .style("font-size", "16px")
-            .text("Confirmed");
-
-          for(var i = 1; i < 10; i++) {
-            // add legend indicator
-            svg.append("rect")
-              .attr("y", 8 * margin.top + i * LINE_HEIGHT - INDICATOR_SIZE + 3)
-              .attr("x", width - margin.top * 2 - INDICATOR_SIZE - 3)
-              .attr("width", INDICATOR_SIZE)
-              .attr("height", INDICATOR_SIZE)
-              .style("fill", colorScale(totalScale(totalDomain[i-1])));
-            svg.append("text")
-              .attr("y", 8 * margin.top + i * LINE_HEIGHT)
-              .attr("x", width - margin.top * 2)
-              .attr("text-anchor", "start")
-              .style("font-size", "16px")
-              .text(totalDomain[i-1]);
           }
+        })
+        .attr("d", path)
+        .on("mouseover", function(d) {
+          tooltip.transition()
+            .duration(200)
+            .style("opacity", 0.9);
+          tooltip.html(function() {
+            var selectedState = statesData.find(function(element) { return element.state === d.properties.name})
+            var content = `
+              <table style="margin-top: 2.5px;">
+                <tr><td>State: </td><td style="text-align: right">` + selectedState.state + `</td></tr>
+                <tr><td>Case: </td><td style="text-align: right">` + selectedState.cases + `</td></tr>
+              </table>`;
+            return content;
+          })
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY) + "px");
+        })
+        .on("mouseout", function() {
+          tooltip.transition()
+          .duration(500)
+          .style("opacity", 0);
+        });
+      // add mesh of states
+      svg.append("path")
+        .datum(topojson.mesh(mapData, mapData.objects.states, (a, b) => a !== b))
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-linejoin", "round")
+        .attr("d", path);
+      // add legend title
+      svg.append("text")
+        .attr("y", height * legendY)
+        .attr("x", width * legendX)
+        .attr("text-anchor", "left")
+        .style("font-size", legendFontSize)
+        .text("Confirmed");
+      // add legend indicator
+      for(let i = 0; i < legendDomain.length; i++) {
+        // add legend indicator
+        svg.append("rect")
+          .attr("y", height * legendY + (i + 1) * legendBase * (legendSize + legendSpace) - legendBase * legendSize)
+          .attr("x", width * legendX)
+          .attr("width", legendBase * legendSize)
+          .attr("height", legendBase * legendSize)
+          .style("fill", colorScale(casesScale(legendDomain[i])));
+        svg.append("text")
+          .attr("y", height * legendY + (i + 1) * width * (legendSize + legendSpace))
+          .attr("x", width * (legendX + legendSize + legendSpace))
+          .attr("text-anchor", "start")
+          .style("font-size", legendFontSize)
+          .text(function() {
+            if (i === legendDomain.length -1) {
+              return legendDomain[i] + "+"
+            } else {
+              return legendDomain[i] + " - " + legendDomain[i+1]
+            }
+          });
+      }
     }
   }
 }
@@ -156,13 +166,11 @@ path:hover {
 
 div.tooltip {
   position: absolute;
-  text-align: center;
-  width: 80px;
-  height: 14px;
-  padding: 2px;
-  font: 12px sans-serif;
-  background: #fff;
-  border: 0px;
-  pointer-events: none;
+  line-height: 1;
+  padding: 6px;
+  background: rgba(0, 0, 0, 0.7);
+  color: #fff;
+  border-radius: 4px;
+  font-size: 12px;
 }
 </style>
